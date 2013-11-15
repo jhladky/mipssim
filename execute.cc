a59fa2a1edc53001bb0b24d6f9cebcc7e330e7ff
Jacob Hladky <jacob.hladky@gmail.com>
2013-11-15 14:40:40 -0800

    with debug printouts

--- execute.cc
+++ execute.cc
@@ -1,4 +1,5 @@
 #include "mipsim.hpp"
+#include <cstdlib>
 #include <iostream>
 
 Stats stats;
@@ -11,6 +12,7 @@ unsigned int signExtend16to32ui(short i) {
 void execute() {
    static unsigned int pctemp;
    static bool doJump = false;
+   static int count = 0;
    Data32 instr = imem[pc];
    GenericType rg(instr);
    RType rt(instr);
@@ -20,6 +22,7 @@ void execute() {
    unsigned int addr;
    
    if(doJump) {
+      cout << "newLoc: " << pctemp << endl;
       pc = pctemp;
       doJump = false;
    } else {
@@ -27,15 +30,23 @@ void execute() {
    }
    
    stats.instrs++;
+   if(count++ == 200) {
+      exit(1);
+   }
    switch(rg.op) {
    case OP_SPECIAL:
       switch(rg.func) {
       case SP_ADDU:
          stats.numRType++;
          stats.numRegWrites++;
-         stats.numRegReads++; //+= 2?
+         stats.numRegReads += 2; //+= 2?
          
+         cout << "Value of RS in ADDU: " << rt.rs << endl;
+         cout << "Value of RT in ADDU: " << rt.rt << endl;
+         cout << "Arg RS to ADDU: " << rf[rt.rs] << endl;
+         cout << "Arg immed to ADDU: " << rf[rt.rt] << endl;
          rf.write(rt.rd, rf[rt.rs] + rf[rt.rt]);
+         cout << "Result of ADDU: " << rf[rt.rd] << endl;
          break;
       case SP_SLL:
          stats.numRType++;
@@ -57,7 +68,6 @@ void execute() {
          stats.numRegWrites++;
 
          rf.write(31, pc + 4);
-         //pctarget = rf[rt.rd];
          pctemp = rf[rt.rd];
          doJump = true;
          break;
@@ -72,8 +82,13 @@ void execute() {
       stats.numIType++;
       stats.numRegWrites++;
       stats.numRegReads++;
-      
+
+      cout << "Value of RS in ADDIU: " << ri.rs << endl;
+      cout << "Value of RT in ADDIU: " << ri.rt << endl;
+      cout << "Arg RS to ADDIU: " << rf[ri.rs] << endl;
+      cout << "Arg immed to ADDIU: " << signExtend16to32ui(ri.imm) << endl;
       rf.write(ri.rt, rf[ri.rs] + signExtend16to32ui(ri.imm));
+      cout << "Result of ADDIU: " << rf[ri.rt] << endl;
       break;
    case OP_SW:
       stats.numIType++;
@@ -98,10 +113,13 @@ void execute() {
       stats.numRegReads++;
       stats.numRegWrites++;
       
-      if(rf[ri.rt] < signExtend16to32ui(ri.imm)) {
-         rf.write(ri.rs, 1);
+
+      if(rf[ri.rs] < signExtend16to32ui(ri.imm)) {
+         rf.write(ri.rt, 1U);
+         cout << "SLTI: TRUE, ri.rt is: " <<  rf[ri.rt] << endl << endl;
       } else {
-         rf.write(ri.rs, 0);
+         rf.write(ri.rt, 0U);
+         cout << "SLTI: FALSE, ri.rt is: " <<  rf[ri.rt] << endl << endl;
       }
       
    case OP_LUI: //IMPLEMENT ME!!!
@@ -121,25 +139,32 @@ void execute() {
       stats.numRegReads += 2;
       
       if(rf[ri.rs] == rf[ri.rt]) {
-         //pctarget += (ri.imm << 2);
-         pctemp = pctarget + (ri.imm << 2);
+         pctemp = pc + (signExtend16to32ui(ri.imm) << 2);
+         cout << "OP_BEQ setting new pc to: " << pctemp << endl;
          doJump = true;
       }
       break;
    case OP_BNE: //IMPLEMENT ME!!!
       stats.numIType++;
       stats.numRegReads += 2;
-      
+
+      cout << "Value of RS in BNE: " << ri.rs << endl;
+      cout << "Value of RT in BNE: " << ri.rt << endl;
+      cout << "Arg RS to BNE: " << rf[ri.rs] << endl;
+      cout << "Arg RT to BNE: " << rf[ri.rt] << endl;
       if(rf[ri.rs] != rf[ri.rt]) {
-         //pctarget += (ri.imm << 2);
-         pctemp = pctarget + (ri.imm << 2);
+         pctemp = pc + (signExtend16to32ui(ri.imm) << 2);
+         cout << "OP_BNE setting new pc to: "  << pctemp << endl;
          doJump = true;
       }
+      
       break;
    case OP_J: //IMPLEMENT ME!!! (done possibly)
       stats.numJType++;
       
-      pctemp = rj.target << 2;
+      pctemp = (pc & 0xf0000000) | (rj.target << 2);
+      cout << "OP_J setting new pc to: "  << pctemp << endl;
+      
       doJump = true;
       break;
    case OP_JAL: //IMPLEMENT ME!!!
@@ -147,8 +172,8 @@ void execute() {
       stats.numRegWrites++;
       
       rf.write(31, pc + 4);
-      //pctarget = (pc & 0xf0000000) | rj.target << 2;
-      pctemp = rj.target << 2;
+      pctemp = (pc & 0xf0000000) | (rj.target << 2);
+      cout << "OP_JAL setting new pc to: "  << pctemp << endl;
       doJump = true;
       break;
    default:
@@ -158,3 +183,11 @@ void execute() {
       break;
    }
 }
+
+
+//Executing: 400188: op: OP_J target: 10006f
+/*
+  24 bits, 
+we need 32 bits
+
+ */
